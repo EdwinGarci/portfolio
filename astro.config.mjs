829b1ce14d6f1e2f7475bdfa9fbc6d7941b9c988
EdwinGarci <edwingarciatavara@gmail.com>
2024-12-27 12:42:15 -0500

    Add sitemap integration and robots.txt for improved SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import tailwind from '@astrojs/tailwind';
 
 import netlify from '@astrojs/netlify';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fidel-gt-dev.netlify.app/',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   output: 'server',
   adapter: netlify()
 });
