5d892c5706555b10c09b89b9c25b83f5423ff259
EdwinGarci <edwingarciatavara@gmail.com>
2024-12-27 10:14:48 -0500

    Replace Node adapter with Netlify adapter and change output configuration for deployment compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fidel-gt-dev.netlify.app/',
   integrations: [tailwind()],
-  output: 'static',
-  adapter: node({
-    mode: 'standalone'
-  })
+  output: 'server',
+  adapter: netlify()
 });
